0d53fd2313015fc43747f120e4f58f909e96cbf3
Matthias Wanner <matthias.wanner@chello.at>
2015-06-02 14:06:05 +0200

    geolocation not finished... Centering must be done

--- www/js/map.js
+++ www/js/map.js
@@ -407,18 +407,44 @@ function initialize()
 /* initialized finished*/
 }
 
-/* geolocation
+/* geolocation */
 
 
 function getLocation() {
     if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(showPosition);
+        navigator.geolocation.getCurrentPosition(showPosition, showError);
     } else {
         x.innerHTML = "Geolocation is not supported by this browser.";
     }
+}
 
+function showPosition(position) {
+    var latitude  = position.coords.latitude;
+    var longitude = position.coords.longitude;
+    document.getElementById("long").innerHTML = longitude;
+    document.getElementById("lat").innerHTML = latitude;
 
-} */
+    var center = new google.maps.LatLng(latitude, longitude);
+    map.panTo(center);
+
+}
+
+function showError(error) {
+    switch(error.code) {
+        case error.PERMISSION_DENIED:
+            x.innerHTML = "User denied the request for Geolocation."
+            break;
+        case error.POSITION_UNAVAILABLE:
+            x.innerHTML = "Location information is unavailable."
+            break;
+        case error.TIMEOUT:
+            x.innerHTML = "The request to get user location timed out."
+            break;
+        case error.UNKNOWN_ERROR:
+            x.innerHTML = "An unknown error occurred."
+            break;
+    }
+}
 
 var stages = [
     ['Mainstage', 47.823007, 13.175547, 9, '<p class="pag">Main Stage</p><a href="../sites/stages/mainstage.html"style="font-family: elfFont">Timetable</a>'],
