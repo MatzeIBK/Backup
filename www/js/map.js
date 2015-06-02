e297fdfb04f1ff02dd4e8afd092e1583569ba049
Matthias Wanner <matthias.wanner@chello.at>
2015-06-02 20:53:56 +0200

    geo test

--- www/js/map.js
+++ www/js/map.js
@@ -409,6 +409,49 @@ function initialize()
 /* initialized finished*/
 }
 
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition, showError);
+    } else {
+        x.innerHTML = "Geolocation is not supported by this browser.";
+    }
+}
+
+function showPosition(position) {
+    var latitude  = position.coords.latitude;
+    var longitude = position.coords.longitude;
+    var img_center = '../img/max/center_icon.png';
+    var userCoordinates = new google.maps.LatLng(latitude, longitude);
+    document.getElementById("center").addEventListener("click", function () {
+        document.getElementById("long").innerHTML = longitude;
+        document.getElementById("lat").innerHTML = latitude;
+
+        var center = new google.maps.Marker({
+            position: userCoordinates,
+            map: map,
+            icon: img_center
+        });
+        map.setCenter({lat: latitude, lng: longitude});
+    });
+}
+
+function showError(error) {
+    switch(error.code) {
+        case error.PERMISSION_DENIED:
+            x.innerHTML = "User denied the request for Geolocation."
+            break;
+        case error.POSITION_UNAVAILABLE:
+            x.innerHTML = "Location information is unavailable."
+            break;
+        case error.TIMEOUT:
+            x.innerHTML = "The request to get user location timed out."
+            break;
+        case error.UNKNOWN_ERROR:
+            x.innerHTML = "An unknown error occurred."
+            break;
+    }
+}
+
 var stages = [
     ['Mainstage', 47.823007, 13.175547, 9, '<p class="pag">Main Stage</p><a href="../sites/stages/mainstage.html"style="font-family: elfFont">Timetable</a>'],
     ['Clubcircus', 47.822759, 13.173946, 2, '<p class="pag">Club Circus</p><a href="../sites/stages/clubcircus.html"style="font-family: elfFont">Timetable</a>'],
